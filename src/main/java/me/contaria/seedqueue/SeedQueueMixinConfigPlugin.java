614cda4faa7e811b58f744be2d82c6871b8e0448
contaria <kingcontaria@gmail.com>
2025-06-22 00:30:16 +0200

    cleanup: remove sodium mac mixin config plugin code

--- src/main/java/me/contaria/seedqueue/SeedQueueMixinConfigPlugin.java
+++ src/main/java/me/contaria/seedqueue/SeedQueueMixinConfigPlugin.java
@@ -2,31 +2,10 @@ package me.contaria.seedqueue;
 
 import com.bawnorton.mixinsquared.api.MixinCanceller;
 import me.contaria.speedrunapi.mixin_plugin.SpeedrunMixinConfigPlugin;
-import net.fabricmc.loader.api.FabricLoader;
 
 import java.util.List;
 
-/**
- * SeedQueues Mixin Config Plugin, extends SpeedrunAPI's plugin to inherit its functionality.
- * Only used for compatibility with Sodium Mac, will be redundant once Sodium is updated to not need the seperate mac version anymore.
- */
 public class SeedQueueMixinConfigPlugin extends SpeedrunMixinConfigPlugin implements MixinCanceller {
-    private static final boolean MAC_SODIUM = FabricLoader.getInstance().isModLoaded("sodiummac");
-
-    @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        // Sodium Mac "Compat"
-        // Create RenderCache Async doesn't apply on Sodium Mac
-        if (MAC_SODIUM) {
-            if (mixinClassName.startsWith(this.mixinPackage + ".compat.sodium.profiling")) {
-                return false;
-            }
-            if (mixinClassName.equals(this.mixinPackage + ".compat.sodium.ClientChunkManagerMixin") || mixinClassName.equals(this.mixinPackage + ".compat.sodium.ChunkBuilder$WorkerRunnableMixin2")) {
-                return false;
-            }
-        }
-        return super.shouldApplyMixin(targetClassName, mixinClassName);
-    }
 
     @Override
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
